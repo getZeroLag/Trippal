3607253a453504bcbd06544c731d2aa5e4ad8f70
getZeroLag <explorezerolag@gmail.com>
2025-01-09 17:00:51 +0530

    init: Setup nextjs, tailwindcss, shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="mx-auto flex h-20 max-w-screen-md items-center justify-between px-6">
+      <h1>Content</h1>
+    </div>
+  );
+}
---
+++ src/components/Appbar.tsx
@@ -0,0 +1,9 @@
+const Appbar = () => {
+  return (
+    <div className="flex items-center justify-between h-20 max-w-screen-md px-6 mx-auto">
+      Appbar
+    </div>
+  );
+};
+
+export default Appbar;
